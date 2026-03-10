054e6c6f64ed05cdf5d346d2ae6291af9cdb9add
Nalinisutha96 <nalinisutha96@gmail.com>
2026-03-10 18:03:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function addTask(){
+
+let task=document.getElementById("taskInput").value;
+
+if(task==="") return;
+
+let li=document.createElement("li");
+
+li.textContent=task;
+
+li.onclick=function(){
+this.remove();
+}
+
+document.getElementById("taskList").appendChild(li);
+
+document.getElementById("taskInput").value="";
+
+}
